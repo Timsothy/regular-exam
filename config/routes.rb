bb83f653956d6a80d40a49e818cade9695e94c34
Timsothy <timothy.ms591@gmail.com>
2023-12-18 11:15:36 +0900

    テスト詳細表示画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'students/show/:id', to: 'students#show', as: 'student', constraints: { id: /\d+/ }
   devise_for :students
   root to: "tests#index"
   resources :tests
